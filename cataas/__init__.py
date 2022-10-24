80aef0b0e3c0ceaaa280e2e55b01c3a2c4e88838
Victor .E <75646316+victorq0q@users.noreply.github.com>
2022-10-24 15:34:12 -0300

    Update __init__.py

--- cataas/__init__.py
+++ cataas/__init__.py
@@ -3,10 +3,10 @@ from random import choice
 
 
 try:
-	check_api = requests.get("https://cataas.com/")
+    check_api = requests.get("https://cataas.com/")
 except Exception as e:
-	print(">> Cataas API erro <<")
-	print(e)
+    print(">> Cataas API erro <<")
+    print(e)
 
 API_URL = "https://cataas.com/cat" 
 
@@ -18,21 +18,21 @@ tag_list = requests.get("https://cataas.com/api/tags").json()
 
 def GetData(url_method="", params=None):
 	if not params:
-		params = {"json": "true"}
+	   params = {"json": "true"}
 	elif type(params) == dict:
-		params["json"] = "true"
+	   params["json"] = "true"
 
 	data = requests.get(f"{API_URL}{url_method}", params=params)
 
 	error_msg = "Cataas API\n Data not found!, check if the api is working\n or check if the api has a new version = > https://cataas.com/"
 	if data.status_code == 404 or data.headers["Content-Type"] != "application/json":
-		print(error_msg)
+	   print(error_msg)
 	
 	elif len(data.json()) == 0:
-		print(error_msg)
+	    print(error_msg)
 
 	else:
-		return data.json()
+	    return data.json()
 
 
 def Cat(tag=False, text=False, cat_type=False, img_filter=False, width=False, heigth=False):
@@ -40,19 +40,19 @@ def Cat(tag=False, text=False, cat_type=False, img_filter=False, width=False, he
 	params = {}  
 							
 	if tag in tag_list:
-		methods = f"{methods}/{tag}"
+	    methods = f"{methods}/{tag}"
 	if text:
-		methods = f"{methods}/says/{text}"
+	    methods = f"{methods}/says/{text}"
 
 	if cat_type in cat_type_list:
-		params["type"] = cat_type
+	    params["type"] = cat_type
 	if img_filter in image_filter_list:
-		params["filter"] = img_filter
+	    params["filter"] = img_filter
 
 	if width:
-		params["width"] = width
+	    params["width"] = width
 	if heigth:
-		params["heigth"] = heigth
+	    params["heigth"] = heigth
 
 	resp = GetData(url_method=methods, params=params) #final url: https://cataas.com/{methods}?{params}
 	resp["url"] = "https://cataas.com" + resp["url"]
@@ -68,10 +68,10 @@ def Random(text=False, img_filter=False):
 	params["type"] = choice(cat_type_list)
 
 	if text:
-		methods = f"{methods}/says/{text}"
+	   methods = f"{methods}/says/{text}"
 
 	if img_filter in image_filter_list:
-		params["filter"] = choice(image_filter_list)
+	   params["filter"] = choice(image_filter_list)
 
 	resp = GetData(url_method=methods, params=params)
 	resp["url"] = "https://cataas.com" + resp["url"]
